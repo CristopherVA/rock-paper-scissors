d7f72fb4fa62afb2fc34ce50d577fa7ce9266de2
Brandon Fernandez <fernandez.b@dhn.com.do>
2022-08-05 16:58:31 -0400

    game done

--- components/MainButton.js
+++ components/MainButton.js
@@ -53,6 +53,12 @@ function MainButton() {
                Rules
             </Link>
          </div>
+
+         <div onClick={() => dispatch(setReset()) } className='absolute bottom-8 left-8 px-5 py-1 border-4 rounded-lg border-white  cursor-pointer text-white ' >
+            <Link href='/game'>
+               Reset
+            </Link>
+         </div>
       </>
    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import { useDispatch } from 'react-redux'
 import Layout from '../components/Layout'
 import MainButton from '../components/MainButton'
+import { setReset } from '../redux/slices/playSlice'
 
 export default function Home() {
+
+  const dispatch = useDispatch()
 
   return (
     <>
       <Layout>
           <MainButton />   
+
+          
       </Layout>
 
     </>
